ff02689f6187211194374e07d4b0d11fb6c68847
RavitejaUppulapu <21951a05n3@iare.ac.in>
2024-01-23 22:48:20 +0530

    level-9

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const app = express();
+const csurf = require("csurf");
+const cookieParser = require("cookie-parser");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
 const path = require("path");
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("Shh! It's a secret!"));
+app.use(csurf({ cookie: true }));
 
 app.set("view engine", "ejs");
-
-app.get("/", async function (request, response) {
+app.get("/", async (request, response) => {
   const allTodos = await Todo.getTodos();
+  const overdues = await Todo.overDue();
+  const duesLater = await Todo.dueLater();
+  const duesToday = await Todo.dueToday();
+  const completedTodos = await Todo.completedTodos();
+
   if (request.accepts("html")) {
-    response.render("index", { allTodos });
+    response.render("index", {
+      allTodos,
+      overdues,
+      duesLater,
+      duesToday,
+      completedTodos,
+      csrfToken: request.csrfToken(),
+    });
   } else {
-    response.json(allTodos);
+    response.json({ allTodos, overdues, duesLater, duesToday, completedTodos });
   }
 });
 
@@ -23,8 +39,14 @@ app.get("/", function (request, response) {
 });
 
 app.get("/todos", async function (_request, response) {
+  console.log("Processing list of all Todos ...");
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
   try {
-    const todos = await Todo.findAll(); // Querying all Todos from the database
+    const todos = await Todo.findAll();
     return response.json(todos);
   } catch (error) {
     console.log(error);
@@ -45,17 +67,21 @@ app.get("/todos/:id", async function (request, response) {
 app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    if (request.accepts("html")) {
+      return response.redirect("/");
+    } else {
+      return response.json(todo);
+    }
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async function (request, response) {
+app.put("/todos/:id", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
-    const updatedTodo = await todo.markAsCompleted();
+    const updatedTodo = await todo.setCompletionStatus(request.body.completed);
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
@@ -64,13 +90,16 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 });
 
 app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
+
   try {
-    const result = await Todo.destroy({
-      where: {
-        id: request.params.id,
-      },
-    });
-    return response.json(result === 1); // If one row is affected, deletion was successful
+    const delTodo = await Todo.deleteTodo(request.params.id);
+    response.send(delTodo ? true : false);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -7,23 +7,80 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
     }
 
-    static addTodo({ title, dueDate }) {
-      return Todo.create({
-        title: title,
-        dueDate: dueDate,
+    static async addTodo(params) {
+      return this.create({
+        title: params.title,
+        dueDate: params.dueDate,
         completed: false,
       });
     }
+
     static getTodos() {
       return this.findAll();
     }
 
+    static overDue() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
+          completed: {
+            [Op.eq]: false,
+          },
+        },
+      });
+    }
+
+    static dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
+          completed: {
+            [Op.eq]: false,
+          },
+        },
+      });
+    }
+
+    static dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+          completed: {
+            [Op.eq]: false,
+          },
+        },
+      });
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
+    }
+
+    setCompletionStatus(status) {
+      return this.update({ completed: !status });
+    }
+
+    static completedTodos() {
+      return this.findAll({
+        where: {
+          completed: true,
+        },
+      });
+    }
+
+    static async deleteTodo(id) {
+      return await Todo.destroy({ where: { id: id } });
     }
   }
   Todo.init(
